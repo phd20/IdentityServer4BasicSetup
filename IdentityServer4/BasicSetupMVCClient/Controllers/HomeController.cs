ade5b185353d59c9808b6f6064785c6859e9554a
PhD20 <dmprofessor227@gmail.com>
2018-10-28 20:25:13 -0500

    Using the access token

--- IdentityServer4/BasicSetupMVCClient/Controllers/HomeController.cs
+++ IdentityServer4/BasicSetupMVCClient/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using BasicSetupMVCClient.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace BasicSetupMVCClient.Controllers
 {
@@ -41,6 +44,18 @@ namespace BasicSetupMVCClient.Controllers
         {
             await HttpContext.SignOutAsync("Cookies");
             await HttpContext.SignOutAsync("oidc");
+        }
+
+        public async Task<IActionResult> CallApiUsingUserAccessToken()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = await client.GetStringAsync("http://localhost:5001/identity");
+
+            ViewBag.Json = JArray.Parse(content).ToString();
+            return View();
         }
     }
 }
